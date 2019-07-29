d2aeab609e9119cf948af7d9b2c386404e556770
akuz1218 <aleksei.kuzmenko@netcracker.com>
2019-07-29 16:02:02 +0400

    init commit

---
+++ first/src/main/java/com/lowton/refactoring/first/Customer.java
@@ -0,0 +1,67 @@
+package com.lowton.refactoring.first;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+
+    public Customer(String _name) {
+        this._name = _name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String staitment() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+        String result = "Прокат " + getName() + "\n";
+
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentals.nextElement();
+
+            // Определение суммы для каждой строки
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENSS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            // Начисление бонусных очков
+            frequentRenterPoints++;
+
+            // Бонус за двухдневный прокат новинки
+            if ((each.getMovie().getPriceCode() == Movie.NEW) && each.getDaysRented() > 1)
+                frequentRenterPoints++;
+
+            // Вывод результатов для каждого проката
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+
+        // Добавление колонтитула
+
+        result += "Сумма задолженности: " + String.valueOf(totalAmount) + "\n";
+        result += "Вы заработали " + String.valueOf(frequentRenterPoints) + "бонусных очков!\n";
+        return result;
+    }
+}
