dd419f04cae26b9562af1056087f180cb5680a96
akuz1218 <aleksei.kuzmenko@netcracker.com>
2019-07-29 16:45:30 +0400

    Move method amountFor(...) from Customer to Rental class

--- first/src/main/java/com/lowton/refactoring/first/Customer.java
+++ first/src/main/java/com/lowton/refactoring/first/Customer.java
@@ -50,23 +50,6 @@ public class Customer {
     }
 
     private double amountFor(Rental aRental) {
-        double result = 0;
-        // Определение суммы для каждой строки
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENSS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return aRental.getCharge();
     }
 }
