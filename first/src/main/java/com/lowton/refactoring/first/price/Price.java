546323a94e62d6808892d4945a1a525f18539a5a
akuz1218 <aleksei.kuzmenko@netcracker.com>
2019-07-29 18:34:49 +0400

    Move method getFrequentRenterPoints() to Price class

--- first/src/main/java/com/lowton/refactoring/first/price/Price.java
+++ first/src/main/java/com/lowton/refactoring/first/price/Price.java
@@ -4,6 +4,12 @@ import com.lowton.refactoring.first.Movie;
 
 public abstract class Price {
     public abstract int getPriceCode();
-
     public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
