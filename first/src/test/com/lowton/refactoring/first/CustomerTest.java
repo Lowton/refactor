945ce2e72c98a5d9a54ecf6ed461634e87a99dbe
akuz1218 <aleksei.kuzmenko@netcracker.com>
2019-07-29 17:32:54 +0400

    Add test for method htmlStatement()

--- first/src/test/com/lowton/refactoring/first/CustomerTest.java
+++ first/src/test/com/lowton/refactoring/first/CustomerTest.java
@@ -7,12 +7,12 @@ class CustomerTest {
 
     @Test
     void staitment() {
-        String johnStaitment = "Прокат John\n" +
+        String johnStatement = "Прокат John\n" +
                 "\tRats\t8.0\n" +
                 "\tCats\t6.0\n" +
                 "Сумма задолженности: 14.0\n" +
                 "Вы заработали 3 бонусных очков!\n";
-        String aliceStaitment = "Прокат Alice\n" +
+        String aliceStatement = "Прокат Alice\n" +
                 "\tDogs\t7.5\n" +
                 "Сумма задолженности: 7.5\n" +
                 "Вы заработали 1 бонусных очков!\n";
@@ -28,7 +28,34 @@ class CustomerTest {
         john.addRental(new Rental(cats, 2));
         alice.addRental(new Rental(dogs, 7));
 
-        Assertions.assertEquals(johnStaitment, john.statement());
-        Assertions.assertEquals(aliceStaitment, alice.statement());
+        Assertions.assertEquals(johnStatement, john.statement());
+        Assertions.assertEquals(aliceStatement, alice.statement());
+    }
+
+    @Test
+    void htmlStaitmentTest() {
+        String johnStatement = "<h1>Прокат <em>John</em></h1>" +
+                "<p>Rats: 8.0</p>\n" +
+                "<p>Cats: 6.0</p>\n" +
+                "<p>Сумма задолженности <em>14.0</em></p>\n" +
+                "<p>Вы заработали <em>3</em> бонусных очков.</p>";
+        String aliceStatement = "<h1>Прокат <em>Alice</em></h1>" +
+                "<p>Dogs: 7.5</p>\n" +
+                "<p>Сумма задолженности <em>7.5</em></p>\n" +
+                "<p>Вы заработали <em>1</em> бонусных очков.</p>";
+
+        Movie cats = new Movie("Cats", 1);
+        Movie dogs = new Movie("Dogs", 2);
+        Movie rats = new Movie("Rats", 0);
+
+        Customer john = new Customer("John");
+        Customer alice = new Customer("Alice");
+
+        john.addRental(new Rental(rats, 6));
+        john.addRental(new Rental(cats, 2));
+        alice.addRental(new Rental(dogs, 7));
+
+        Assertions.assertEquals(johnStatement, john.htmlStatement());
+        Assertions.assertEquals(aliceStatement, alice.htmlStatement());
     }
 }
